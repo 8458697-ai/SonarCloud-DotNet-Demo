7b3b2295a5a85aa7b50dc02bfa6f014efbb12c32
8458697-ai <8458697@stud.kai.edu.ua>
2026-01-14 22:18:43 +0200

    Update Program.cs

--- NetSdrClientApp/Program.cs
+++ NetSdrClientApp/Program.cs
@@ -1,10 +1,9 @@
 using System;
 
-namespace WrongNamespace {
+namespace NetSdrClientApp {
     public static class Program {
         public static void Main(string[] args) {
-            // Спеціально ламаємо синтаксис для червоного хрестика
-            Це помилка для звіту лаби 5!!! 
+            Console.WriteLine("Hello, Lab 5 is working!");
         }
     }
 }
