f00778149d3c8182efe6f98027b85c6c6f3e659b
8458697-ai <8458697@stud.kai.edu.ua>
2026-01-14 22:14:44 +0200

    Update Program.cs

--- NetSdrClientApp/Program.cs
+++ NetSdrClientApp/Program.cs
@@ -1,22 +1,10 @@
 using System;
 
-namespace WrongNamespace { // ТЕСТ МАЄ ВПАСТИ ЧЕРЕЗ ЦЕЙ РЯДОК
+namespace WrongNamespace {
     public static class Program {
         public static void Main(string[] args) {
-            ExecuteClientOperations();
-        }
-
-        public static void ExecuteClientOperations() {
-            Console.WriteLine("--- Client Operations Initialized ---");
-            try {
-                for (int i = 0; i < 10; i++) {
-                    Console.WriteLine("Client Action #" + i);
-                }
-            }
-            catch (Exception ex) {
-                Console.WriteLine("Client side exception: " + ex.Message);
-            }
-            Console.WriteLine("--- Client Operations Completed ---");
+            // Спеціально ламаємо синтаксис для червоного хрестика
+            Це помилка для звіту лаби 5!!! 
         }
     }
 }
